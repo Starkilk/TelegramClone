17592a7301d83fead3e38065f60be9d07f806fb0
Starlik <pavel.bolt@bk.ru>
2023-10-02 19:58:08 +0300

    onResume()

--- app/src/main/java/com/pasha/telegramclone/ui/ChatsFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/ChatsFragment.kt
@@ -14,9 +14,14 @@ class ChatsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentChatsBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
     }
 
 
