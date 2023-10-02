d8eff2024e4f4708381ddeadd9b60bb60bc251aa
Starlik <pavel.bolt@bk.ru>
2023-10-02 19:38:49 +0300

    created a ChatsFragment

---
+++ app/src/main/java/com/pasha/telegramclone/ui/ChatsFragment.kt
@@ -0,0 +1,23 @@
+package com.pasha.telegramclone.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pasha.telegramclone.R
+import com.pasha.telegramclone.databinding.FragmentChatsBinding
+
+
+class ChatsFragment : Fragment() {
+    private lateinit var binding:FragmentChatsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+}
