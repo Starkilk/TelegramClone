dd78a2a4fc4f977020c63a8468d04e3e21d7c282
Starlik <pavel.bolt@bk.ru>
2023-10-05 21:51:56 +0300

    onCreateOptionsMenu()

--- app/src/main/java/com/pasha/telegramclone/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/fragments/SettingsFragment.kt
@@ -3,6 +3,8 @@ package com.pasha.telegramclone.ui.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pasha.telegramclone.R
@@ -13,7 +15,10 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
-
+        setHasOptionsMenu(true)//включили наше меню в контексте
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)//определили меню на этом фрагменте
+    }
 }
