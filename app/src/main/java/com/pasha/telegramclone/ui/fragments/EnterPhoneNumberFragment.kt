65a349347ba807d75aaa039e4149129134a39ad7
Starlik <pavel.bolt@bk.ru>
2023-10-08 23:58:03 +0300

    checking the phone number

--- app/src/main/java/com/pasha/telegramclone/ui/fragments/EnterPhoneNumberFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/fragments/EnterPhoneNumberFragment.kt
@@ -5,12 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.pasha.telegramclone.R
+import com.pasha.telegramclone.databinding.FragmentEnterPhoneNumberBinding
 
 
-class EnterPhoneNumberFragment : Fragment(R.layout.fragment_enter_phone_number) {
+class EnterPhoneNumberFragment : Fragment() {
 
+private lateinit var binding: FragmentEnterPhoneNumberBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEnterPhoneNumberBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onStart() {
+        super.onStart()
+        binding.bRegisterNext.setOnClickListener { sendCode() }
+    }
+
+    //функция проверки номера телефона
+    private fun sendCode() {
+        //если номер не введён:
+        if(binding.registerInputPhoneNumber.text.toString().isEmpty()){
+            Toast.makeText(activity, getString(R.string.register_toast_enter_phone),Toast.LENGTH_SHORT).show()
+        }else{
+            //если введён, то открываем: EnterCodeFragment
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.registerDataContainer,EnterCodeFragment())
+                ?.addToBackStack(null)//записали в back stack
+                ?.commit()
+        }
+    }
 
 
 }
