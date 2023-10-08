704695b6732ac9dffe2a5294417993028166240d
Starlik <pavel.bolt@bk.ru>
2023-10-08 23:59:52 +0300

    fun onStart() - open EnterPhoneNumberFragment

--- app/src/main/java/com/pasha/telegramclone/activities/RegisterActivity.kt
+++ app/src/main/java/com/pasha/telegramclone/activities/RegisterActivity.kt
@@ -2,15 +2,32 @@ package com.pasha.telegramclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import com.pasha.telegramclone.R
 import com.pasha.telegramclone.databinding.ActivityRegisterBinding
+import com.pasha.telegramclone.ui.fragments.EnterPhoneNumberFragment
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+
+    //при старте данного активити открываем фрагмент: EnterPhoneNumberFragment
+    override fun onStart() {
+        super.onStart()
+        mToolbar = binding.registerToolbar//инициализировали тулбар
+        setSupportActionBar(mToolbar)//привязали тулбар
+        title = getString(R.string.register_title_your_phone)//установили заголовок на тулбар
+
+        //при старте данного активити открываем фрагмент: EnterPhoneNumberFragment
+        supportFragmentManager.beginTransaction()
+            .add(R.id.registerDataContainer, EnterPhoneNumberFragment())
+            .commit()
     }
 }
