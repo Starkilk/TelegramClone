36b63cc3b9bef9ed2373c5975a362ca6f79ef082
Starlik <pavel.bolt@bk.ru>
2023-10-08 23:53:42 +0300

    added logic related to user authorization verification(demo)

--- app/src/main/java/com/pasha/telegramclone/activities/MainActivity.kt
+++ app/src/main/java/com/pasha/telegramclone/activities/MainActivity.kt
@@ -1,8 +1,10 @@
-package com.pasha.telegramclone
+package com.pasha.telegramclone.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import com.pasha.telegramclone.R
 import com.pasha.telegramclone.databinding.ActivityMainBinding
 import com.pasha.telegramclone.ui.fragments.ChatsFragment
 import com.pasha.telegramclone.ui.objects.AppDrawer
@@ -26,11 +28,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFunc() {
-        setSupportActionBar(mToolbar)//передаём НАШ тулбар на место тулбара по умолчанию
-        mAppDrawer.create()//вызвали методы, которы находятся в нашем классе AppDrawer
-        //при запускек активити - открыть фрагмент с чатами
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.dataContainer, ChatsFragment()).commit()
+        //проверка на авторизованность пользователя
+        if(false){
+            //если АВТОРИЗОВАН
+            setSupportActionBar(mToolbar)//передаём НАШ тулбар на место тулбара по умолчанию
+            mAppDrawer.create()//вызвали методы, которы находятся в нашем классе AppDrawer
+            //при запускек активити - открыть фрагмент с чатами
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.dataContainer,
+                    ChatsFragment())
+                .commit()
+        }else{
+            //если НЕ АВТОРИЗОВАН
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
     }
 
