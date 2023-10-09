98c7a9aefd82bdf5335767543bd9e628dab8b947
Starlik <pavel.bolt@bk.ru>
2023-10-09 18:44:55 +0300

    exodus from account

--- app/src/main/java/com/pasha/telegramclone/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/fragments/SettingsFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import com.pasha.telegramclone.R
+import com.pasha.telegramclone.activities.MainActivity
+import com.pasha.telegramclone.activities.RegisterActivity
 import com.pasha.telegramclone.databinding.FragmentSettingsBinding
+import com.pasha.telegramclone.utilits.AUTH
+import com.pasha.telegramclone.utilits.replaceActivity
 
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
@@ -21,5 +26,16 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)//определили меню на этом фрагменте
+    }
+
+    //функция для действий при нажатии на пункты иеню на тулбаре настроек
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){//слошаем что нажал юзер
+            R.id.settingsMenuExit->{//если нажал выйти
+                AUTH.signOut()//выходим из аккаунта
+                (activity as MainActivity).replaceActivity(RegisterActivity())
+            }
+        }
+        return true
     }
 }
