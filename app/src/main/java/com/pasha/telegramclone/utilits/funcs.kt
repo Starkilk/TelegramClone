b36e8f3c5916935d14723a7c144e6f52db40675b
Starlik <pavel.bolt@bk.ru>
2023-10-09 14:00:24 +0300

    file for template functions

---
+++ app/src/main/java/com/pasha/telegramclone/utilits/funcs.kt
@@ -0,0 +1,35 @@
+package com.pasha.telegramclone.utilits
+
+import android.content.Intent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.pasha.telegramclone.R
+import com.pasha.telegramclone.activities.RegisterActivity
+import com.pasha.telegramclone.ui.fragments.ChatsFragment
+
+//шаблонная функция которая фозволяет вызывать Toast во фрагментах
+fun Fragment.showToast(message: String){
+    Toast.makeText(this.context,message,Toast.LENGTH_SHORT).show()
+}
+
+//шаблонная функция для переключения между АКТИВИТИ
+fun AppCompatActivity.replaceActivity(activity: AppCompatActivity){
+    val intent = Intent(this, activity::class.java)
+    startActivity(intent)
+    this.finish()
+}
+
+//шаблонная функцияя для переключения между ФРАГМЕНТАМИ ИЗ АКТИВИТИ
+fun AppCompatActivity.replaceFragment(fragment: Fragment){
+    supportFragmentManager.beginTransaction()
+        .addToBackStack(null)
+        .replace(R.id.dataContainer, fragment).commit()
+}
+
+//шаблонная функцияя для переключения между ФРАГМЕНТАМИ ИЗ ФРАГМЕНТА
+fun Fragment.replaceFragment(fragment: Fragment){
+    this.parentFragmentManager.beginTransaction()
+        .addToBackStack(null)
+        .replace(R.id.dataContainer,fragment).commit()
+}
