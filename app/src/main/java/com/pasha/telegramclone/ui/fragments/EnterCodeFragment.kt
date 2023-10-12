b352062387d6575ee06bed41b5e72fabd0b85067
Starlik <pavel.bolt@bk.ru>
2023-10-12 14:47:36 +0300

    firebaseDatabase-not working:FIX IT LATER

--- app/src/main/java/com/pasha/telegramclone/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/fragments/EnterCodeFragment.kt
@@ -5,19 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
 import com.pasha.telegramclone.activities.MainActivity
 import com.pasha.telegramclone.activities.RegisterActivity
 import com.pasha.telegramclone.databinding.FragmentEnterCodeBinding
 import com.pasha.telegramclone.utilits.AUTH
 import com.pasha.telegramclone.utilits.AppTextWatcher
+import com.pasha.telegramclone.utilits.CHILD_ID
+import com.pasha.telegramclone.utilits.CHILD_PHONE
+import com.pasha.telegramclone.utilits.CHILD_USERNAME
+import com.pasha.telegramclone.utilits.NODE_USERS
+import com.pasha.telegramclone.utilits.REF_DATABASE_ROOT
 import com.pasha.telegramclone.utilits.replaceActivity
 import com.pasha.telegramclone.utilits.showToast
 
 
-class EnterCodeFragment(val mPhoneNumber: String, val id: String) : Fragment() {
+class EnterCodeFragment(val phoneNumber: String, val id: String) : Fragment() {
     private lateinit var binding: FragmentEnterCodeBinding
 
 
@@ -31,7 +34,7 @@ class EnterCodeFragment(val mPhoneNumber: String, val id: String) : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        (activity as RegisterActivity).title = mPhoneNumber
+        (activity as RegisterActivity).title = phoneNumber
         //слушатель введённого текста в EditText c помощью выведеного нами в отдельный класс:TextWatcher(а)
         binding.registerInputCode.addTextChangedListener(AppTextWatcher {//передаём лямда функцию
             val string = binding.registerInputCode.text.toString()
@@ -43,15 +46,34 @@ class EnterCodeFragment(val mPhoneNumber: String, val id: String) : Fragment() {
     }
 
     private fun enterCode() {
-        val code =  binding.registerInputCode.text.toString()//получаем код который юзер ввёл в EditText
-        val credential = PhoneAuthProvider.getCredential(id,code)//передаём id и code
+        val code = binding.registerInputCode.text.toString()//получаем код который юзер ввёл в EditText
+        val credential = PhoneAuthProvider.getCredential(id, code)//передаём id и code
 
-            //авторизация/регистрация пользователя
+        //авторизация/регистрация пользователя
         AUTH.signInWithCredential(credential).addOnCompleteListener { task ->
             if (task.isSuccessful) {//если позьзователь авторизировался и всё хорошо
-                showToast("Welcome!")
-                (activity as RegisterActivity).replaceActivity(MainActivity())
-            } else showToast(task.exception?.message.toString())//если произошла ошибка
+                /*val uid = AUTH.currentUser?.uid.toString()//проинициализировали id
+                //вписываем все данные о пользователи в объект mutableMapOf, чтобы передать этот объект в БД
+                //это всё выглядит наглядно в самом Firebase(сайт)
+                val dateMap = mutableMapOf<String, Any>()
+                dateMap[CHILD_ID] = uid
+                dateMap[CHILD_PHONE] = phoneNumber
+                dateMap[CHILD_USERNAME] = uid
+
+                //ЭТА СТРОЧКА ЛОМАЕТ
+                //передали данные о пользователе в БД(создали ноду id пользователя и у него заполнили все данные)
+                REF_DATABASE_ROOT.child(NODE_USERS).child(uid).updateChildren(dateMap)
+                    .addOnCompleteListener { taskSecond ->
+                       //действия после занесенияя данных в БД
+                        if (taskSecond.isSuccessful) {//если task2.всё хорошо, то :*/
+                            showToast("Welcome!")
+                            (activity as RegisterActivity).replaceActivity(MainActivity())
+                        //} else showToast("Mistake")//если не всё хорошо, то покажет проблему
+
+
+                    }else showToast("Mistake")//если произошла ошибка
+
+
         }
     }
 }
