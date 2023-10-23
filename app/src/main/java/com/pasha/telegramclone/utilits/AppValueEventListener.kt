1f611f1ba7a6591650dbbe2c42e774aef8feae6b
Starlik <pavel.bolt@bk.ru>
2023-10-23 22:09:30 +0300

    comment

--- app/src/main/java/com/pasha/telegramclone/utilits/AppValueEventListener.kt
+++ app/src/main/java/com/pasha/telegramclone/utilits/AppValueEventListener.kt
@@ -4,6 +4,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 
+//вынесли в отдельный класс слушатель информации
 class AppValueEventListener(val onSucces:(DataSnapshot) ->Unit):ValueEventListener{
     override fun onDataChange(snapshot: DataSnapshot) {
         onSucces(snapshot)
