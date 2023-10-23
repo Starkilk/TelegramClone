9873b7b07bcb7158582f274041b0a966e918a2a2
Starlik <pavel.bolt@bk.ru>
2023-10-23 22:13:00 +0300

    removed inheritance from BaseFragment

--- app/src/main/java/com/pasha/telegramclone/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/fragments/SettingsFragment.kt
@@ -11,14 +11,25 @@ import android.view.ViewGroup
 import com.pasha.telegramclone.R
 import com.pasha.telegramclone.activities.MainActivity
 import com.pasha.telegramclone.activities.RegisterActivity
+import com.pasha.telegramclone.databinding.ActivityMainBinding
+import com.pasha.telegramclone.databinding.FragmentChangeNameBinding
 import com.pasha.telegramclone.databinding.FragmentSettingsBinding
 import com.pasha.telegramclone.utilits.AUTH
+import com.pasha.telegramclone.utilits.USER
 import com.pasha.telegramclone.utilits.replaceActivity
 import com.pasha.telegramclone.utilits.replaceFragment
 
 
-class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+class SettingsFragment : Fragment() {
 
+private lateinit var binding:FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding  = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
     override fun onStart() {
         super.onStart()
         //при запуске фрагмента настроек Draewer отключается
@@ -29,6 +40,16 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
         super.onResume()
         setHasOptionsMenu(true)//включили наше меню в контексте
 
+        initFields()//инициализация текстовых блоков в окне настроек
+    }
+
+    //инициализация всех текстView в окне настроек(взятые из БД , помещённые в объект User данные, добавили во вьюшки)
+    private fun initFields() {
+        binding.settingsBio.text = USER.bio
+        binding.settingsFullName.text = USER.fullname
+        binding.settingsPhoneNumber.text = USER.phone
+        binding.settingStatus.text = USER.status
+        binding.settingsUsername.text = USER.username
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
