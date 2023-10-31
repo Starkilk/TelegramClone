d8942cdef2858bc22494806d806f3fa2de54fe2c
Starlik <pavel.bolt@bk.ru>
2023-10-31 20:06:50 +0300

    fix memory leaks

--- app/src/main/java/com/pasha/telegramclone/ui/fragments/ContactsFragment.kt
+++ app/src/main/java/com/pasha/telegramclone/ui/fragments/ContactsFragment.kt
@@ -1,17 +1,13 @@
 package com.pasha.telegramclone.ui.fragments
 
 import android.os.Bundle
-import android.provider.ContactsContract.Data
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.view.drawToBitmap
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.pasha.telegramclone.R
 import com.pasha.telegramclone.databinding.ContactItemBinding
@@ -25,6 +21,7 @@ import com.pasha.telegramclone.utilits.NODE_USERS
 import com.pasha.telegramclone.utilits.REF_DATABASE_ROOT
 import com.pasha.telegramclone.utilits.downloadAndSetImage
 import com.pasha.telegramclone.utilits.getCommonModel
+import com.pasha.telegramclone.utilits.replaceFragment
 import de.hdodenhof.circleimageview.CircleImageView
 
 
@@ -37,6 +34,9 @@ private lateinit var mRecyclerView: RecyclerView
 private lateinit var mAdapter: FirebaseRecyclerAdapter<CommonModel, ContactsHolder>//Firebase адаптер
 private lateinit var mRefContacts:DatabaseReference//откуда скачивает данные(путь к нужной ноде и тд)
 private lateinit var mRefUsers:DatabaseReference//путь к юзерам, чтобы с найти пользователей из тел. книги и заполнить их данными разметку contact_item
+private lateinit var mRefUsersListener:AppValueEventListener//объект - слушатель
+private var mapListeners = hashMapOf<DatabaseReference, AppValueEventListener>()//хэш-таблица, хронящая слушатели данных БД
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -56,11 +56,12 @@ private lateinit var mRefUsers:DatabaseReference//путь к юзерам, чт
 
     private fun initRecyclerView() {
         mRecyclerView = binding.contactsRecyclerView//ининициализация рецайклера
-        mRefContacts = REF_DATABASE_ROOT.child(NODE_PHONES_CONTACTS).child(CURRENT_UID)
+        mRefContacts = REF_DATABASE_ROOT.child(NODE_PHONES_CONTACTS).child(CURRENT_UID)//путь
 
         val options = FirebaseRecyclerOptions.Builder<CommonModel>()
             .setQuery(mRefContacts, CommonModel::class.java)//запрос на считываение из Firebase(путь, куда сохраняем)
             .build()
+
         mAdapter = object :FirebaseRecyclerAdapter<CommonModel,ContactsHolder>(options){
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsHolder {
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)//надули разметку
@@ -73,19 +74,27 @@ private lateinit var mRefUsers:DatabaseReference//путь к юзерам, чт
                 model: CommonModel
             ) {
                 mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS).child(model.id)//переходим в пользователя из телефонной книги
-                mRefUsers.addValueEventListener(AppValueEventListener{
 
+                //присвоили слушатель
+                mRefUsersListener = AppValueEventListener {
                     val contact = it.getCommonModel()//получили данные контакта, записав их в модель
 
                     //заполнем item контакта
                     holder.name.text = contact.fullname
                     holder.status.text = contact.state
                     holder.photo.downloadAndSetImage(contact.photoUrl)
-                })
+                    //добавляем слушатель нажатия на каждого пользователя. при нажатии открываем фрагмент чата и пиередаём на него информацию о нажатом пользователе
+                    holder.itemView.setOnClickListener { replaceFragment(SingleChatFragment(contact)) }
+                }
 
+                //применили слушатель
+                //слушатель, наблюдает за элементами, чтобы  отображать изменения данных(Firebase) других пользователей
+                mRefUsers.addValueEventListener(mRefUsersListener)
 
+                mapListeners[mRefUsers] = mRefUsersListener//таблица пользователей изменения которых надо слушать
             }
         }
+
         mRecyclerView.adapter = mAdapter//присвоили адаптер
         mAdapter.startListening()//подключили слушатель к адаптеру
     }
@@ -102,6 +111,12 @@ private lateinit var mRefUsers:DatabaseReference//путь к юзерам, чт
     override fun onPause() {
         super.onPause()
         mAdapter.stopListening()
+        //устранили утечку памяти
+        //чистим слушатели, когда пользователь перестаёт наблюдать окно контактов
+        mapListeners.forEach{
+            it.key.removeEventListener(it.value)
+        }
+
     }
 }
 
